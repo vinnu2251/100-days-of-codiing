d6832822871fbd0c590673f33e351eea06647568
vinay chitturi <vinay@vinays-MacBook-Pro.local>
2024-03-28 12:51:52 +0530

    Day - 01 message

---
+++ Day-01/bandname.py
@@ -0,0 +1,8 @@
+print("======== WELCOME TO BAND NAME GENERATOR========")
+
+home_town = input("please enter your home town name : ")
+pet_name= input("please enter your petname: ")
+
+band_name = home_town + pet_name
+
+print(f"you can keep the band name as {band_name}")
